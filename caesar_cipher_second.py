b5020163911722227e038c0db2bc5777dbe6a56d
Aradhya Baliyan <aradhya.baliyann829@gmail.com>
2026-05-29 10:26:40 +0530

    Implement caesar_cipher with string module

---
+++ caesar_cipher_second.py
@@ -0,0 +1,45 @@
+import string
+
+
+def main():
+    while True:
+        key = input("Enter a key: ")
+        if 0 < int(key) < 26:
+            key = int(key)
+            break
+
+
+    message = input("Enter a message: ")
+    while True:
+        status = input("Do you want to (e)ncrypt or (d)ecrypt?")
+        if status == "e" or status == "d":
+            break
+
+    if status == "e":
+        # shifted string
+        shifted_lower = string.ascii_lowercase[key:] + string.ascii_lowercase[:key]
+        shifted_upper = string.ascii_uppercase[key:] + string.ascii_uppercase[:key]
+
+        cipher = str.maketrans(string.ascii_lowercase, shifted_lower)
+        cipher.update(str.maketrans(string.ascii_uppercase, shifted_upper))
+
+        print(message)
+        print("Encrypted message: {}".format(message.translate(cipher)))
+
+    if status == "d":
+
+        #shifted string
+        shifted_lower =
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
