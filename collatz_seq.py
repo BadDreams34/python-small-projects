c6cb0987e49165b2c259282bfb8a553f2e7b153d
Aradhya Baliyan <aradhya.baliyann829@gmail.com>
2026-06-03 09:33:04 +0530

    Add time sleep function and Quitting of program

--- collatz_seq.py
+++ collatz_seq.py
@@ -1,6 +1,8 @@
 """Collatz Sequence
 3n+1 problem """
 
+import time
+import sys
 print("Collatz sequence is a sequence of numbers in which \n"
       "The next number is n/2 if n = even \n"
       "The next number is n * 3 + 1 if n = odd \n"
@@ -9,9 +11,12 @@ print("Collatz sequence is a sequence of numbers in which \n"
 
 def main(): #main function
     #ask for the number
-    print("Enter the number for which Collatz sequence is needed to be generated")
+    print("Enter the number for which Collatz sequence is needed to be generated(greater than 0) or QUIT")
     while True:
         n = input("> ")
+        if n.upper().startswith("Q"):
+            print("Program terminated")
+            sys.exit()
         if not n.isdecimal():
             print("Please enter the correct value for n (It should be integer)")
             continue
@@ -27,10 +32,11 @@ def main(): #main function
             break
         if next_num % 2 == 0:
             next_num = int(next_num/2)
-            print(f", {next_num}", end="")
+            print(f", {next_num}", end="", flush=True)
         elif next_num % 2 == 1:
             next_num = int(3 * next_num + 1)
-            print(f", {next_num}", end="")
+            print(f", {next_num}", end="",flush=True)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
