9fa9cf765ef775af94f75ba9399e45908a4b5577
Aradhya Baliyan <aradhya.baliyann829@gmail.com>
2026-06-05 10:29:02 +0530

    Implement cave length with random shifts

---
+++ deep_cave__updated.py
@@ -0,0 +1,46 @@
+"""Deep Cave
+Program generate an endless animation simulating a cave"""
+
+import random, sys, time
+
+#Set up the constants
+WIDTH = 50
+PAUSE_AMOUNT = 0.45
+
+
+print("Deep Cave")
+print("Press Control + C to stop")
+time.sleep(2)
+
+
+left_width = 20
+cave_len = 10
+
+while True:
+    right_width = WIDTH - left_width - cave_len
+    print("#"* left_width + cave_len * " " + "#" * right_width)
+
+    try:
+        time.sleep(PAUSE_AMOUNT)
+    except KeyboardInterrupt: #checks for keyboard interrupt
+        print("Program Ended")
+        sys.exit()
+
+    #Adjust the left Width
+    dice_roll = random.randint(1,6)
+    if dice_roll == 1 and left_width > 1:
+        left_width -=1 #decreases the left width
+    elif dice_roll == 2 and left_width < WIDTH - 1:
+        left_width +=1
+    else:
+        pass
+
+    #Adjust the cave_len
+    dice_roll = random.randint(1,6)
+    if dice_roll == 1 and cave_len > 1:
+        cave_len -= 1
+    elif dice_roll == 2 and left_width + cave_len < WIDTH - 1:
+        cave_len += 1
+    else:
+        pass
+
